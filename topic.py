9c7d449f82bba360d73557edae49d5c4327bf0ec
alansun17904 <asun17904@gmail.com>
2023-06-28 10:09:39 -0400

    auth added to topic upload

--- topic.py
+++ topic.py
@@ -1,9 +1,12 @@
+import os
 import tqdm
 from transformers import pipeline
 from datasets import load_dataset
 
+import datasets
+datasets.logging.set_verbosity_error()
 
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli", device=0)
 # load the dataset
 dataset = load_dataset("asun17904/bank_examples")
 candidate_labels = ["finance", "river"]
@@ -18,4 +21,5 @@ def classify_example(example, candidate_labels):
 
 
 dataset = dataset.map(lambda x: classify_example(x, candidate_labels), batched=True)
-dataset.push_to_hub("asun17904/bank_examples_with_labels")
+token = os.environ["HF_TOKEN"]
+dataset.push_to_hub("asun17904/bank_examples_with_labels", token=token)
